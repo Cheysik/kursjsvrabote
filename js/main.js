2e126eaaa8e7bacaf7c6e0adc4c81f7b933edc82
Vladislav <example@gmail.com>
2023-08-25 19:36:06 +0200

    dobavlenie otpravki dannych iz form na server

--- js/main.js
+++ js/main.js
@@ -212,4 +212,56 @@ window.addEventListener('DOMContentLoaded', function() {
         ".menu .container"
     ).render();
 
+    //Forms
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Загрузка',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+    
+    function postData(form) {
+        form.addEventListener('submit', (e)=> {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            
+            request.setRequestHeader('Content-type', 'application/json')// zagolovok ustanavlivat' ne nuzno esli dannie ne v formate JSON, esli ustanovit ne dla JSONa, budet oshibka
+            const formData = new FormData(form); //vsegda ukazyvat' atribut name v inputah HTML
+            
+            const object = {};
+            formData.forEach(function(value, key){
+                object[key] = value;
+            });
+
+            const json = JSON.stringify(object);
+
+            request.send(json); //request.send(formData);
+
+            request.addEventListener('load', () =>{
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000);
+                } else{
+                    statusMessage.textContent = message.error;
+                }
+            })
+        });
+    }
 });
